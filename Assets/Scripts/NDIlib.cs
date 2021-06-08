51741456465abac7e6c699f1fbd318e94dd2bee6
edom18 <edo.m18@gmail.com>
2021-06-08 11:06:49 +0900

    feat: Added new enum defines and a struct and a method.

--- Assets/Scripts/NDIlib.cs
+++ Assets/Scripts/NDIlib.cs
@@ -9,6 +9,44 @@ using UnityEngine;
 
 public static class NDIlib
 {
+    public enum recv_color_format_e
+    {
+        // No alpha channel: BGRX Alpha channel: BGRA
+        recv_color_format_BGRX_BGRA = 0,
+
+        // No alpha channel: UYVY Alpha channel: BGRA
+        recv_color_format_UYVY_BGRA = 1,
+
+        // No alpha channel: RGBX Alpha channel: RGBA
+        recv_color_format_RGBX_RGBA = 2,
+
+        // No alpha channel: UYVY Alpha channel: RGBA
+        recv_color_format_UYVY_RGBA = 3,
+
+        // On Windows there are some APIs that require bottom to top images in RGBA format. Specifying
+        // this format will return images in this format. The image data pointer will still point to the
+        // "top" of the image, althought he stride will be negative. You can get the "bottom" line of the image
+        // using : video_data.p_data + (video_data.yres - 1)*video_data.line_stride_in_bytes
+        recv_color_format_BGRX_BGRA_flipped = 200,
+
+        // Read the SDK documentation to understand the pros and cons of this format.
+        recv_color_format_fastest = 100,
+
+        // Legacy definitions for backwards compatibility
+        recv_color_format_e_BGRX_BGRA = recv_color_format_BGRX_BGRA,
+        recv_color_format_e_UYVY_BGRA = recv_color_format_UYVY_BGRA,
+        recv_color_format_e_RGBX_RGBA = recv_color_format_RGBX_RGBA,
+        recv_color_format_e_UYVY_RGBA = recv_color_format_UYVY_RGBA
+    }
+
+    public enum recv_bandwidth_e
+    {
+        recv_bandwidth_metadata_only = -10,
+        recv_bandwidth_audio_only = 10,
+        recv_bandwidth_lowest = 0,
+        recv_bandwidth_highest = 100,
+    }
+
     [StructLayout(LayoutKind.Sequential)]
     public struct find_create_t
     {
@@ -22,6 +60,19 @@ public static class NDIlib
     {
         public IntPtr p_ndi_name;
         public IntPtr p_url_address;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct recv_create_v3_t
+    {
+        public source_t source_to_connect_to;
+        public recv_color_format_e color_format;
+        public recv_bandwidth_e bandwidth;
+
+        [MarshalAs(UnmanagedType.U1)]
+        public bool allow_video_fields;
+
+        public IntPtr p_ndi_recv_name;
     }
 
     public class Source
@@ -96,6 +147,9 @@ public static class NDIlib
 
     [DllImport("Processing.NDI.Lib.x64.dll", EntryPoint = "NDIlib_find_get_current_sources", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr find_get_current_sources(IntPtr p_instance, ref UInt32 p_no_sources);
+
+    [DllImport("Processing.NDI.Lib.x64.dll", EntryPoint = "NDIlib_recv_create_v3", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr recv_create_v3(ref recv_create_v3_t p_create_settings);
 
     public static string Utf8ToString(IntPtr nativeUtf8, uint? length = null)
     {
