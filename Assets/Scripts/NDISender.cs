14fa8c08de33a4d1887932cf8c9cada6007840f6
edom18 <edo.m18@gmai.com>
2021-11-04 21:08:57 +0900

    feat: Implemented a sender (this is temporary commit).

--- Assets/Scripts/NDISender.cs
+++ Assets/Scripts/NDISender.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Runtime.InteropServices;
+using Klak.Ndi;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,11 +11,15 @@ namespace NDISample
     public class NDISender : MonoBehaviour
     {
         [SerializeField] private string _ndiName;
+        [SerializeField] private ComputeShader _encodeCompute;
 
         private IntPtr _sendInstance;
+        private FormatConverter _formatConverter;
 
         private void Start()
         {
+            _formatConverter = new FormatConverter(_encodeCompute);
+
             IntPtr nname = Marshal.StringToHGlobalAnsi(_ndiName);
             NDIlib.SendSettings sendSettings = new NDIlib.SendSettings { NdiName = nname };
             _sendInstance = NDIlib.send_create(sendSettings);
@@ -51,7 +57,7 @@ namespace NDISample
 
             ScreenCapture.CaptureScreenshotIntoRenderTexture(tempRT);
 
-            ComputeBuffer converted = null;
+            ComputeBuffer converted = _formatConverter.Encode(tempRT, false, false);
 
             RenderTexture.ReleaseTemporary(tempRT);
 
@@ -60,7 +66,31 @@ namespace NDISample
 
         unsafe void OnReadback(AsyncGPUReadbackRequest request)
         {
-            
+            // Ignore errors.
+            if (request.hasError) return;
+
+            // Ignore it if the NDI object has been already disposed.
+            if (_sendInstance == null || _sendInstance == IntPtr.Zero) return;
+
+            // Readback data retrieval
+            var data = request.GetData<byte>();
+            var pdata = NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(data);
+
+            // Data size verification
+            if (data.Length / sizeof(uint) !=
+                FrameDataCount(_width, _height, _enableAlpha)) return;
+
+            // Frame data setup
+            var frame = new NDIlib.video_frame_v2_t
+            {
+                xres = _width, yres = _height, line_stride_in_bytes = _width * 2,
+                FourCC = NDIlib.FourCC_type_e.FourCC_type_UYVY,
+                frame_format_type = NDIlib.frame_format_type_e.frame_format_type_progressive,
+                p_data = (System.IntPtr)pdata, p_metadata = IntPtr.Zero,
+            };
+
+            // Send via NDI
+            NDIlib.send_send_video_async_v2(_sendInstance, frame);
         }
     }
 }
