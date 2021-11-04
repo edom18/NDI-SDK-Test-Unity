fccfbe425b87fd6c48406dac17043060f0dddb85
edom18 <edo.m18@gmail.com>
2021-11-05 01:08:11 +0900

    feat: Added a config class to declear the DLL names for each platform.

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,16 @@
+namespace NDISample
+{
+    public static class Config
+    {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        public const string DllName = "Processing.NDI.Lib.x64";
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        public const string DllName = "libndi.4";
+#elif UNITY_ANDROID
+        // TODO: あとでチェック
+        public const string DllName = "Processing.NDI.Lib.so";
+#else
+        public const string DllName = "__Internal";
+#endif
+    }
+}
